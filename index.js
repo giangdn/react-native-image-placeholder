143c10503ef5b502a7b0edc9fc91ebb7af5b97cd
Ngoc Giang <giang.ngocdao@gmail.com>
2020-09-04 12:09:22 +0700

    change to using react-native-fast-image

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Image, ImageBackground, ActivityIndicator, View } from "react-native";
+import { ImageBackground, ActivityIndicator, View } from "react-native";
+import Image from "react-native-fast-image";
 
 const lfsr = require("lfsr");
 
@@ -35,69 +36,98 @@ class ImageLoad extends React.Component {
     });
   }
 
-  render() {
+  _renderLoader() {
     const {
-      style,
-      source,
       mode,
-      resizeMode,
       borderRadius,
       backgroundColor,
-      children,
       loadingStyle,
       placeholderSource,
       placeholderStyle,
       customImagePlaceholderDefaultStyle
     } = this.props;
     return (
-      <ImageBackground
-        onLoadEnd={this.onLoadEnd.bind(this)}
-        onError={this.onError.bind(this)}
-        style={[styles.backgroundImage, style]}
-        source={source}
-        resizeMode={resizeMode}
-        borderRadius={borderRadius}
+      <View
+        style={[
+          styles.viewImageStyles,
+          { borderRadius: borderRadius },
+          backgroundColor ? { backgroundColor: backgroundColor } : {}
+        ]}
       >
-        {this.state.isLoaded && !this.state.isError ? (
-          children
-        ) : (
-          <View
-            style={[
-              styles.viewImageStyles,
-              { borderRadius: borderRadius },
-              backgroundColor ? { backgroundColor: backgroundColor } : {}
-            ]}
+        {this.props.isShowActivity && !this.state.isError && (
+          <ActivityIndicator
+            style={styles.activityIndicator}
+            size={loadingStyle ? loadingStyle.size : "small"}
+            color={loadingStyle ? loadingStyle.color : "gray"}
+          />
+        )}
+        <Image
+          style={
+            placeholderStyle
+              ? placeholderStyle
+              : [
+                  styles.imagePlaceholderStyles,
+                  customImagePlaceholderDefaultStyle,
+                  mode === "blur" ? styles.imageBlurStyle : {}
+                ]
+          }
+          source={
+            placeholderSource
+              ? placeholderSource
+              : mode === "blur"
+              ? require(`./Images/blur/blur-${lfsr.seq(3)}.jpg`)
+              : require("./Images/empty-image.png")
+          }
+        ></Image>
+      </View>
+    );
+  }
+
+  render() {
+    const { style, source, resizeMode, borderRadius, children } = this.props;
+    return (
+      <View
+        style={{
+          borderRadius: borderRadius,
+          position: "relative",
+          flex: 1
+        }}
+      >
+        {typeof children !== "undefined" && children !== null && (
+          <ImageBackground
+            onLoadEnd={this.onLoadEnd.bind(this)}
+            onError={this.onError.bind(this)}
+            style={[styles.backgroundImage, style]}
+            source={source}
+            resizeMode={resizeMode}
+            borderRadius={borderRadius}
           >
-            {this.props.isShowActivity && !this.state.isError && (
-              <ActivityIndicator
-                style={styles.activityIndicator}
-                size={loadingStyle ? loadingStyle.size : "small"}
-                color={loadingStyle ? loadingStyle.color : "gray"}
+            {!this.state.isLoaded && this._renderLoader()}
+            {children && (
+              <View style={styles.viewChildrenStyles}>{children}</View>
+            )}
+          </ImageBackground>
+        )}
+        {(typeof children === "undefined" || children === null) && (
+          <View style={styles.viewImageStyles}>
+            {this.state.isLoaded && !this.state.isError && (
+              <Image
+                onLoadEnd={this.onLoadEnd.bind(this)}
+                onError={this.onError.bind(this)}
+                style={[
+                  styles.fastImageStyle,
+                  { borderRadius: borderRadius },
+                  style
+                ]}
+                source={source}
+                resizeMode={resizeMode}
+                borderRadius={borderRadius}
               />
             )}
-            <Image
-              style={
-                placeholderStyle
-                  ? placeholderStyle
-                  : [
-                      styles.imagePlaceholderStyles,
-                      customImagePlaceholderDefaultStyle
-                    ]
-              }
-              source={
-                placeholderSource
-                  ? placeholderSource
-                  : mode === "blur"
-                  ? require(`./Images/blur/blur-${lfsr.seq(3)}.jpg`)
-                  : require("./Images/empty-image.png")
-              }
-            ></Image>
+            {!this.state.isLoaded && this._renderLoader()}
           </View>
         )}
-        {this.props.children && (
-          <View style={styles.viewChildrenStyles}>{this.props.children}</View>
-        )}
-      </ImageBackground>
+      </View>
     );
   }
 }
@@ -113,9 +143,27 @@ const styles = {
   },
   viewImageStyles: {
     flex: 1,
-    backgroundColor: "#e9eef1",
     justifyContent: "center",
     alignItems: "center"
+  },
+  imageBlurStyle: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
+  fastImageStyle: {
+    width: "100%",
+    height: "100%",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    resizeMode: "contain"
   },
   imagePlaceholderStyles: {
     width: 100,
